e1a5ab51c09d29cd614d9c2d1c27280d3aaec4e0
tyleri <tyler.ishikawa@outlook.com>
2014-07-29 01:48:04 -0400

    prob 4 first attempt

--- problem4.cs
+++ problem4.cs
@@ -3,5 +3,28 @@ using System;
 class problem4
 {
 	public static void Main()
-	{}
+	{
+		int i = 999, j = 999;
+		while (i > 99)
+			while (j > 99)
+				if (testPalin(i*j)
+						{
+						Console.WriteLine(i*j);
+						return;
+						}
+	}
+
+	public static bool testPalin(num)
+	{
+		int[] array = new int[num.ToString().Length];
+		for (int n = 0; n < array.Length; n++)
+		{
+			array[n] = num % 10;
+			num /= 10;
+		}
+
+		if (array.equals(Array.Reverse(array))
+			return true;
+		else
+			return false;
 }
