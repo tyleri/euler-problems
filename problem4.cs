bbfce3ca2b6ca54793006219ce6f883fbc126fda
tyleri <tyler.ishikawa@outlook.com>
2014-07-29 02:11:14 -0400

    Work in progress

--- problem4.cs
+++ problem4.cs
@@ -5,26 +5,28 @@ class problem4
 	public static void Main()
 	{
 		int i = 999, j = 999;
-		while (i > 99)
-			while (j > 99)
-				if (testPalin(i*j)
+		for (; i > 99; i--)
+			for (; j > 99; j--)
+				if (testPalin(i*j))
 						{
 						Console.WriteLine(i*j);
 						return;
 						}
 	}
 
-	public static bool testPalin(num)
+	public static bool testPalin(int num)
 	{
-		int[] array = new int[num.ToString().Length];
+		int testnum = num;
+		int[] array = new int[testnum.ToString().Length];
 		for (int n = 0; n < array.Length; n++)
 		{
-			array[n] = num % 10;
-			num /= 10;
+			array[n] = testnum % 10;
+			testnum /= 10;
 		}
 
-		if (array.equals(Array.Reverse(array))
-			return true;
-		else
-			return false;
+		for (int k = 0; k < array.Length; k++)
+			if (array[k] != array[array.Length-k-1])
+				return false;
+		return true;
+	}
 }
