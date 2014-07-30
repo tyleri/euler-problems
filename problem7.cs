cecfee09b2de26c8144387a6dc61d3fc2bcba211
tyleri <tyler.ishikawa@outlook.com>
2014-07-30 00:16:11 -0400

    problems 7 and 8 complete

---
+++ problem7.cs
@@ -0,0 +1,24 @@
+using System;
+
+class problem7
+{
+	public static void Main()
+	{
+		int count = 2, currentNum = 4;
+		while (count < 10001)
+		{
+			currentNum++;
+			for (int i = 2; i < currentNum; i++)
+			{
+				if (currentNum % i == 0)
+					break;
+				else if (i > currentNum / 2)
+				{
+					count++;
+					break;
+				}
+			}
+		}
+		Console.WriteLine(currentNum);
+	}
+}
