b262fd4aab123431f40879c0060ba3cca19100bc
tyleri <tyler.ishikawa@outlook.com>
2014-08-02 14:46:59 -0400

    Completed Problem 14

---
+++ Problem14.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Problem14
+{
+	public static void Main()
+	{
+		int longest = 0;
+		int start = 0;
+		
+		for (int i = 1; i < 1000000; i++)
+			if (Collatz(i) > longest)
+			{
+				longest = Collatz(i);
+				start = i;
+			}
+		
+		Console.WriteLine(start + " " + longest);
+	}
+
+	public static int Collatz(int start)
+	{
+		double begin = start;
+		int numTerms = 1;
+
+		while (begin != 1)
+		{
+			if (begin % 2 == 0)
+				begin /= 2;
+			else
+				begin = begin * 3 + 1;
+			numTerms++;
+		}
+		
+		return numTerms;
+	}
+}
