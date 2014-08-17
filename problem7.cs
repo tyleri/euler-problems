017c1be992bdc98e264a0827bd67ce6ab40c8813
tyleri <tyler.ishikawa@outlook.com>
2014-08-16 20:52:00 -0400

    Optimized Problem7

--- problem7.cs
+++ problem7.cs
@@ -1,24 +1,24 @@
 using System;
+using System.Diagnostics;
 
 class Problem7
 {
 	public static void Main()
 	{
+		Stopwatch s = Stopwatch.StartNew();
 		int count = 2, currentNum = 4;
 		while (count < 10001)
 		{
 			currentNum++;
-			for (int i = 2; i < currentNum; i++)
-			{
-				if (currentNum % i == 0)
-					break;
-				else if (i > currentNum / 2)
+			for (int i = 2; currentNum % i != 0; i++)
+				if (i > Math.Sqrt(currentNum))
 				{
 					count++;
 					break;
 				}
-			}
 		}
 		Console.WriteLine(currentNum);
+		s.Stop();
+		Console.WriteLine("Time Elapsed: {0}", s.Elapsed);
 	}
 }
